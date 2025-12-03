b2e265906fe654828a4763d3ddb05047c7ca4ac6
Aleem Ahmad <aleemahmada107@gmail.com>
2025-12-04 04:17:32 +0500

    Dep to root

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Aleem A!</h2>;
+  const two = <h2 className="big-heading">Aleem A.</h2>;
   const three = <h3 className="big-heading">I transform ideas into dynamic web experiences.</h3>;
   const four = (
     <>
